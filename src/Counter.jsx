74a5c97f3304c4b27843180a54052c16898a1126
Pavel Vasilev <pv9998286620@yandex.ru>
2022-04-14 13:13:05 +0300

    created store

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Counter = () => {
+    return (
+        <div className={'counter'}>
+            <div className={'btns'}>
+                <button className={'btn'}>+</button>
+                <button className={'btn'}>-</button>
+            </div>
+        </div>
+    );
+};
+
